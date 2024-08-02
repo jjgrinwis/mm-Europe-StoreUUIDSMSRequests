921a5445a02730a7e48269a12290936f246c9e98
John Grinwis <john@grinwis.com>
2024-08-02 21:17:31 +0200

    looking up UUID via AIC backend, log if not found

--- mm/StoreUUIDSMSRequests.sql
+++ mm/StoreUUIDSMSRequests.sql
@@ -1,4 +1,5 @@
 @App:name("StoreUUIDSMSRequests")
+@App:description("Lookup UUID in AIC or use provided sub to record SMS requests per UUID")
 @App:qlVersion("2")
 
 /**
@@ -21,10 +22,10 @@
     The user_id will be used to lookup the UUID which will also be our _key. We can drop the user_id(mobilenumber) for privacy reasons.
     We will only store the sha256 of the user_id. As we can't create a sha digest in the stream worker, let's do that in the EdgeWorker.
 
-    If input is already providing a 'valid' uuid, skip the whole lookup part and just write data to sink which will start the query worker.
     Some endpoints don't provide a user_id like: https://docs.nextreason.com/reference/unify-journeys-api-change-id
     But it should have a valid token. EdgeWorker should get the sub out of the token and feed it into our stream worker.
-
+    If the JSON payload has a 'valid' uuid on this change-id path, skip the whole lookup part and just write data to sink which will start the query worker to store the data.
+    
     Our final collection will contain something like this where the _key is UUID of this user which will match to the identity cloud backend.
     {
       "fraudHits": [
@@ -47,27 +48,58 @@
 
 -- Defines `VerificationCallHTTPSource` stream to process events having an auth_type and user_id
 -- we're only interested in a couple of fields, ignoring the other fields in the json payload.
--- CREATE STREAM VerificationCallSource (auth_type string, user_id string, user_id_sha string, url_path string);
--- uuid might be there, it's optional 
+-- uuid might be there, it's optional.
+-- In a future release we might provide the basic auth string with this JSON
 CREATE SOURCE VerificationCallHTTPSource WITH (type = 'http', map.type='json') (auth_type string, user_id string, user_id_sha string, url_path string, uuid string);
 
 -- Define a sink to publish the data to the external application.
--- For now just using some dummy endpoint to create a uuid, this should be the identity cloud profile lookup page
--- https://techdocs.akamai.com/identity-cloud/docs/view-a-user-profile
-CREATE SINK GetUUIDFromAIC WITH (type='http-call', publisher.url='https://api.grinwis.com/uuid/', method='POST', headers="'Content-Type:application/json'", sink.id="getUUID",map.type='json', map.payload="""{"user_id":"{{user_id}}"}""") (user_id STRING, time long, user_id_sha STRING, url_path string);
+-- https://techdocs.akamai.com/identity-cloud/docs/view-a-user-profile will be used to lookup the UUID based on the user_id which is a mobile number.
+-- entity endpoint needs a x-www-form-urlencoded payload.
+-- we need to set map.type to keyvalue in our http call: https://www.macrometa.com/docs/cep/sink/sink-types/http-call
+-- some special character to deal with normal text: https://www.macrometa.com/docs/cep/sink/sink-mapping/keyvalue
+-- direct API call example: https://paw.pt/iXAzhHnC
+-- took use two evenings to find the correct syntax but should look like this:
+CREATE SINK GetUUIDFromAIC WITH (type='http-call', publisher.url='https://pylon.us-dev.janraincapture.com/entity', 
+method='POST', 
+headers="'Content-Type:application/x-www-form-urlencoded','Authorization:Basic <empty>'",
+sink.id="getUUID",
+map.type='keyvalue',
+map.payload.type_name="user",
+map.payload.attributes="""["uuid"]""",
+map.payload.key_value="""`"{{user_id}}"`""",
+map.payload.key_attribute='`mobileNumber`')
+(user_id STRING, time long, user_id_sha STRING, url_path string);
 
 -- The results from our HTTP call should be available in this response body. 
--- It will contain a json body like {"uuid":"12345"}. So let's get that uuid.
+-- It will contain a json body like:
+/**
+{
+  "result": {
+    "uuid": "208ad487-d938-4914-a4e4-56255f0c288e"
+  },
+  "stat": "ok"
+}
+**/
+
+-- but if there is something wrong, "stat" will have a value of "error".
+
+-- so let's get that result.uuid in our uuid json mapping.
 -- Our input vars are available via trp:<attribute name> so let's map them into our object as we can't point back to our input stream.
+-- https://www.macrometa.com/docs/cep/source/source-mapping/json
 CREATE SOURCE UUIDInfo WITH (type='http-call-response', sink.id='getUUID', map.type='json', 
-map.attributes.uuid = '$.uuid',
+map.attributes.uuid = '$.result.uuid',
+map.attributes.stat = '$.stat',
+map.attributes.error = '$.error_description',
 map.attributes.user_id = 'trp:user_id_sha',
 map.attributes.url_path = 'trp:url_path',
-map.attributes.time = 'trp:time') (uuid string, user_id string, url_path string, time long);
+map.attributes.time = 'trp:time') (uuid string, user_id string, url_path string, time long, stat string, error string);
 
 -- We've reached the end of our stream, just feed this info into our query worker which will store the data.
 -- We don't care about the results but could forward the results into some other output
 CREATE SINK UpdateFraudWorkerStream WITH (type='query-worker', query.worker.name="updateFraud") (key string, fraud object );
+
+-- Our logger sink in case there is some issue
+CREATE SINK LogStream WITH (type="logger", priority='ERROR') (error string);
 
 -- Let's lookup the UUID attached to this user by sending it into http sink doing the lookup.
 -- we can ignore any payload where the auth_type is not sms.
@@ -80,12 +112,18 @@ FROM VerificationCallHTTPSource[auth_type == 'sms' AND uuid is null];
 -- we can ignore any payload where the auth_type is not sms.
 @info(name = 'skip-lookup-uuid')
 INSERT INTO UpdateFraudWorkerStream
-SELECT uuid as key, json:toObject(str:fillTemplate("""{"time": {{1}}, "user_id": "{{2}}", "url_path": "{{3}}"}""", time:timestampInMilliseconds(), user_id, url_path)) AS fraud
+SELECT uuid as key, json:toObject(str:fillTemplate("""{"time": {{1}}, "user_id": "{{2}}", "url_path": "{{3}}"}""", time:timestampInMilliseconds(), user_id_sha, url_path)) AS fraud
 FROM VerificationCallHTTPSource[auth_type == 'sms' AND regex:matches('^[a-fA-F0-9]{8}-(?:[a-fA-F0-9]{4}-){3}[a-fA-F0-9]{12}$', uuid)];
+
+-- if there is some error, send it to our log stream.
+-- make sure streamworkers log stream exists, otherwise logging wont work!
+INSERT INTO LogStream
+SELECT error
+FROM UUIDInfo[stat == 'error'];
 
 -- Let's first create some nice json string via fillTemplate and convert json string to an object.
 -- don't forget to add some "" for fields that are strings!
 @info(name = 'create-json-object')
 INSERT INTO UpdateFraudWorkerStream
 SELECT uuid as key, json:toObject(str:fillTemplate("""{"time": {{1}}, "user_id": "{{2}}", "url_path": "{{3}}"}""", time, user_id, url_path)) AS fraud
-FROM UUIDInfo;
+FROM UUIDInfo[stat != 'error'];
